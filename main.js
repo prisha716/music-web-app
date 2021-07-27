27b582fd006e3dcf43d51f1494a23c550d6df3a8
prisha716 <84237696+prisha716@users.noreply.github.com>
2021-07-27 17:58:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1="";
 song2="";
+song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
 song1=loadSound("music.mp3");
 song2=loadSound("music2.mp3");
@@ -17,7 +22,20 @@ function setup() {
 function modelLoaded() {
     console.log('PoseNet is started');
 }
+function gotPoses(results) {
+     
+    if(results.length > 0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX=" + leftWristxX + "leftWristY=" + leftWristxY);
+       
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
+      }
 
 function draw() {
     image( video,0,0,600,500);
+}
 }
